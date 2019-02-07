82fc4993366edbd87ac361357a20a5e4e97e0e0a
Faith Christensen <faithchristensen111@gmail.com>
2019-02-06 21:10:51 -0800

    Started creating the Popular component, that will list popular programming languages

---
+++ app/components/Popular.js
@@ -0,0 +1,25 @@
+var React = require('react');
+
+class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+        return (
+            <ul className='languages'>
+                {languages.map(function (lang) {
+                    return (
+                        <li key={lang}>
+                            {lang}
+                        </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+}
+
+module.exports = Popular;
